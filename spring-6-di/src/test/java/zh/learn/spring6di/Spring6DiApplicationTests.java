17065964a3ebbcdb63003d91fa861de29ac380c2
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-01-05 11:15:17 +0000

    Section 3: Spring Test Context

--- spring-6-di/src/test/java/zh/learn/spring6di/Spring6DiApplicationTests.java
+++ spring-6-di/src/test/java/zh/learn/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,32 @@
 package zh.learn.spring6di;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import zh.learn.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowiredController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
