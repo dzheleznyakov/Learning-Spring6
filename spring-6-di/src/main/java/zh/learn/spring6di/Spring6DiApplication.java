0b37ea4e356b46bfa7ff3ef4d8800a0903a88202
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-01-05 11:10:28 +0000

    Section 3: The Spring Context

--- spring-6-di/src/main/java/zh/learn/spring6di/Spring6DiApplication.java
+++ spring-6-di/src/main/java/zh/learn/spring6di/Spring6DiApplication.java
@@ -2,12 +2,19 @@ package zh.learn.spring6di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import zh.learn.spring6di.controllers.MyController;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In Main Method");
+		System.out.println(controller.sayHello());
 	}
 
 }
