7a8442913e88cd849ed97ca576c4540958810d4c
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-01-06 17:32:39 +0000

    Section 3: Spring Profiles

---
+++ spring-6-di/src/main/java/zh/learn/spring6di/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,14 @@
+package zh.learn.spring6di.services.i18n;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import zh.learn.spring6di.services.GreetingService;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ spring-6-di/src/test/java/zh/learn/spring6di/controllers/i18n/Myi18NControllerTestEN.java
@@ -0,0 +1,17 @@
+package zh.learn.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myi18NControllerTestEN {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
