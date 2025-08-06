eecc24072b68a955b83818283aff4739ca1841e2
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-06 22:32:11 +0100

    Section 6 - Spring MVC Rest Service: POST method

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
@@ -2,7 +2,13 @@ package zh.learn.spring_6_rest_mvc.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +25,18 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+        Beer savedBeer = beerService.saveNewBeer(beer);
+        return new ResponseEntity(HttpStatusCode.valueOf(HttpStatus.CREATED.value()));
+    }
+
+    @GetMapping
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    @GetMapping(value = "/{beerId}")
     public Beer getBeerById(
             @PathVariable("beerId") UUID beerId
     ) {
