79894bd2ed9bf8aff9f04e3302d31f9863990582
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-06 09:51:06 +0100

    Section 5 - Project Lombok: Project Lombok features

---
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/model/Beer.java
@@ -0,0 +1,20 @@
+package zh.learn.spring_6_rest_mvc.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDated;
+    private LocalDateTime updatedDate;
+}
