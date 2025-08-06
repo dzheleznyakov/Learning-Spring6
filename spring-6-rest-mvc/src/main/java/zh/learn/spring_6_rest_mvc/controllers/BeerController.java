4386411a338a66c2e4068a0e75124a59d5c25fb7
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-06 17:14:23 +0100

    Section 6 - Spring MVC Rest Service: List Operation

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
@@ -2,18 +2,26 @@ package zh.learn.spring_6_rest_mvc.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import zh.learn.spring_6_rest_mvc.model.Beer;
 import zh.learn.spring_6_rest_mvc.services.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+    @RequestMapping("/api/v1/beer/{id}")
     public Beer getBeerById(UUID id) {
         log.debug("Get Beer by Id - in controller");
         return beerService.getBeerById(id);
--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/model/Beer.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/model/Beer.java
@@ -17,6 +17,6 @@ public class Beer {
     private String upc;
     private Integer quantityOnHand;
     private BigDecimal price;
-    private LocalDateTime createdDated;
+    private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
 }
