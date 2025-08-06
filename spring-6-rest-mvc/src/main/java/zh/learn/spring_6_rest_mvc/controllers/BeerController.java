7be5261f120b29fa0e010556da4783410fb487e6
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-06 17:25:57 +0100

    Section 6 - Spring MVC Rest Service: Get by Id

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
@@ -2,7 +2,9 @@ package zh.learn.spring_6_rest_mvc.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import zh.learn.spring_6_rest_mvc.model.Beer;
 import zh.learn.spring_6_rest_mvc.services.BeerService;
@@ -13,17 +15,20 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    @RequestMapping("/api/v1/beer/{id}")
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(
+            @PathVariable("beerId") UUID beerId
+    ) {
         log.debug("Get Beer by Id - in controller");
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
