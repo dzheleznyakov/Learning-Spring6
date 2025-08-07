f2c44b2ae4ac5f0df830da4b2142101840b14643
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-07 15:49:36 +0100

    Section 6 - Spring MVC Rest Service: Assignment 1, add new customer

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/CustomerController.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/CustomerController.java
@@ -2,7 +2,13 @@ package zh.learn.spring_6_rest_mvc.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +24,17 @@ import java.util.UUID;
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
     private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveNewCustomer(customer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedCustomer.getId());
+        return new ResponseEntity(
+                headers,
+                HttpStatusCode.valueOf(HttpStatus.CREATED.value()));
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Customer> listCustomers() {
--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/services/CustomerServiceImpl.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/services/CustomerServiceImpl.java
@@ -59,4 +59,19 @@ public class CustomerServiceImpl implements CustomerService {
         log.debug("Get Customer by Id - in service");
         return this.customerMap.get(id);
     }
+
+    @Override
+    public Customer saveNewCustomer(Customer customer) {
+        Customer savedCustomer = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .name(customer.getName())
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(savedCustomer.getId(), savedCustomer);
+
+        return savedCustomer;
+    }
 }
