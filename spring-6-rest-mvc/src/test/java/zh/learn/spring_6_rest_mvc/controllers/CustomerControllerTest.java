0a0e7c484ce4d78a325f0562f4ce9fb9d2ac116d
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-08-11 21:31:17 +0100

    Section 7 - Spring MockMVC test with Mockito and JUnit: Assignment 12 - Test update customer

--- spring-6-rest-mvc/src/test/java/zh/learn/spring_6_rest_mvc/controllers/CustomerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/zh/learn/spring_6_rest_mvc/controllers/CustomerControllerTest.java
@@ -16,8 +16,10 @@ import java.util.UUID;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,6 +39,18 @@ class CustomerControllerTest {
 
     private final Customer testCustomer = new CustomerServiceImpl().listCustomers().get(0);
     private final Customer testCustomerSaved = new CustomerServiceImpl().listCustomers().get(0);
+
+    @Test
+    void testUpdateCustomer() throws Exception {
+        mockMvc.perform(put("/api/v1/customer/" + testCustomer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testCustomer))
+                )
+                .andExpect(status().isNoContent());
+
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
+    }
 
     @Test
     void testCreateCustomer() throws Exception {
