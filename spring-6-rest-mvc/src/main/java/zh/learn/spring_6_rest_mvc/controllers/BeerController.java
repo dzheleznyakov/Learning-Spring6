f1891b15305b644e98ddb74821ed464cdae56079
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-10-13 14:55:12 +0100

    Sectioin 8 - Controller Advice

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/BeerController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,10 +66,10 @@ public class BeerController {
         return beerService.listBeers();
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNOtFoundException() {
-        return ResponseEntity.notFound().build();
-    }
+//    @ExceptionHandler(NotFoundException.class)
+//    public ResponseEntity handleNOtFoundException() {
+//        return ResponseEntity.notFound().build();
+//    }
 
     @GetMapping(BEER_PATH_ID)
     public Beer getBeerById(
---
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/controllers/ExceptionController.java
@@ -0,0 +1,13 @@
+package zh.learn.spring_6_rest_mvc.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNOtFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+}
