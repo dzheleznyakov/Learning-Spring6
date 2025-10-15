37da0224450cf0ccf331c631ee02e35b65bed8ff
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-10-15 17:46:03 +0100

    Section 9 - Creating JPA Entities

---
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Beer.java
@@ -0,0 +1,37 @@
+package zh.learn.spring_6_rest_mvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zh.learn.spring_6_rest_mvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
---
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Customer.java
@@ -0,0 +1,31 @@
+package zh.learn.spring_6_rest_mvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    private String  name;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
