603cf30c2d4fa1d5b8005192feed6db3ed8e2d23
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-10-15 18:03:56 +0100

    Section 9 - Spring Boot JPA test slice

---
+++ spring-6-rest-mvc/src/test/java/zh/learn/spring_6_rest_mvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,25 @@
+package zh.learn.spring_6_rest_mvc.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import zh.learn.spring_6_rest_mvc.entities.Customer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer savedCustomer = customerRepository.save(Customer.builder()
+                .name("New name")
+                .build());
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+}
