2a4ac6a74504a73852c72c1b26537a6b6bc0a4c4
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-10-15 17:52:43 +0100

    Section 9 - Hibernate UUID id generation

--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Beer.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Beer.java
@@ -1,6 +1,8 @@
 package zh.learn.spring_6_rest_mvc.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 import zh.learn.spring_6_rest_mvc.model.BeerStyle;
 
 import java.math.BigDecimal;
@@ -22,6 +25,9 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Beer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
     @Version
--- spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Customer.java
+++ spring-6-rest-mvc/src/main/java/zh/learn/spring_6_rest_mvc/entities/Customer.java
@@ -1,6 +1,8 @@
 package zh.learn.spring_6_rest_mvc.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,6 +23,9 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Customer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
     @Version
