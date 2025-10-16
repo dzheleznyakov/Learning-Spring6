19957f5a9397efb929b7462c096c13a9a70c1ce9
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2025-10-16 13:04:51 +0100

    Section 9 – Testing for expected exceptions

--- spring-6-rest-mvc/src/test/java/zh/learn/spring_6_rest_mvc/controllers/BeerControllerIT.java
+++ spring-6-rest-mvc/src/test/java/zh/learn/spring_6_rest_mvc/controllers/BeerControllerIT.java
@@ -1,29 +1,33 @@
 package zh.learn.spring_6_rest_mvc.controllers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import zh.learn.spring_6_rest_mvc.entities.Beer;
 import zh.learn.spring_6_rest_mvc.model.BeerDTO;
 import zh.learn.spring_6_rest_mvc.repositories.BeerRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIT {
 
     @Autowired
-    BeerController controller;
+    BeerController beerController;
 
     @Autowired
     BeerRepository beerRepository;
 
     @Test
     void testListBeers() {
-        List<BeerDTO> dtos = controller.listBeers();
+        List<BeerDTO> dtos = beerController.listBeers();
 
         assertThat(dtos.size()).isEqualTo(4);
     }
@@ -33,8 +37,22 @@ class BeerControllerIT {
     @Test
     void testEmptyList() {
         beerRepository.deleteAll();
-        List<BeerDTO> dtos = controller.listBeers();
+        List<BeerDTO> dtos = beerController.listBeers();
 
         assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+        assertThat(dto.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("")
+    void testBeerByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> beerController.getBeerById(UUID.randomUUID()));
     }
 }
